d4466032ac7eca7cc4d257c750c068ba83acba4c
Denis Maciel <denispmaciel@gmail.com>
2024-02-06 06:11:18 +0100

    customize colors

--- lua/no-clown-fiesta/groups/statusline.lua
+++ lua/no-clown-fiesta/groups/statusline.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.highlight(palette, opts)
   return {
-    StatusLine = { fg = palette.gray, bg = palette.bg },
+    StatusLine = { fg = palette.fg, bg = palette.bg },
     StatusLineNC = { fg = palette.alt_bg, bg = palette.alt_bg },
     StatusLineSeparator = { fg = palette.bg },
     StatusLineTerm = { fg = palette.bg },
